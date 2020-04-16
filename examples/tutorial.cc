411f582c759abaa3a40e8fc536f8b28a71ca80fc
alexshakouri <acshakouri@ucdavis.edu>
2020-04-15 23:28:25 -0700

    start the sc2 game and print 'hello'

--- examples/tutorial.cc
+++ examples/tutorial.cc
@@ -1,3 +1,24 @@
-int main(int /*argc*/, char* /*argv*/[]) {
+#include <sc2api/sc2_api.h>
+#include <iostream>
+
+class Bot : public sc2::Agent {
+public:
+	virtual void OnGameStart() final {
+	    std::cout << "Hello, WORLD!" << std::endl;
+	}
+};
+
+int main(int argc, char* argv[]) {
+    sc2::Coordinator coordinator;
+    coordinator.LoadSettings(argc,argv);
+
+    Bot bot;
+    coordinator.SetParticipants({
+        CreateParticipant(sc2::Race::Terran, &bot),
+        CreateComputer(sc2::Race::Zerg)
+    });
+
+    coordinator.LaunchStarcraft();
+    coordinator.StartGame(sc2::kMapBelShirVestigeLE);
     return 0;
 }
